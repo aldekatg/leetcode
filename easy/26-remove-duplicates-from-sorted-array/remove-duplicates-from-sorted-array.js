4013d4fa10d8646e70ecdd1e1e834a0815d852c6
Алдияр <30615940+aldekatg@users.noreply.github.com>
2025-06-13 12:38:09 +0500

    Time: 1 ms (60.11%) | Memory: 56.2 MB (93.40%) - LeetSync

--- easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -7,8 +7,9 @@
     if (nums.length === 0) return 0 
 
     let write = 1
+    let read = 1
 
-    for (let read = 1; read < nums.length; read++) {
+    for (read; read < nums.length; read++) {
         if (nums[read] !== nums[read - 1]) {
             nums[write] = nums[read]
             write++
