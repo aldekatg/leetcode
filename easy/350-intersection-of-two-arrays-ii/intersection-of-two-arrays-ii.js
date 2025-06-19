c116e14e8c3e70d70b54454941622fb8e837f6e1
Алдияр <30615940+aldekatg@users.noreply.github.com>
2025-06-19 16:14:32 +0500

    Time: 3 ms (35.30%) | Memory: 54.3 MB (82.99%) - LeetSync

---
+++ easy/350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ * */
+
+var intersect = function(nums1, nums2) {
+    nums1.sort((a, b) => a - b); // сортируем оба массива
+    nums2.sort((a, b) => a - b);
+
+    let p1 = 0;
+    let p2 = 0;
+    const result = [];
+
+    while (p1 < nums1.length && p2 < nums2.length) {
+        if (nums1[p1] === nums2[p2]) {
+            result.push(nums1[p1]);
+            p1++;
+            p2++;
+        } else if (nums1[p1] < nums2[p2]) {
+            p1++;
+        } else {
+            p2++;
+        }
+    }
+
+    return result;
+};
