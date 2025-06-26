e4302e96455b90e3971ee54deeaf17ee37826685
Алдияр <30615940+aldekatg@users.noreply.github.com>
2025-06-26 12:43:28 +0500

    Time: 21 ms (63.63%) | Memory: 54.9 MB (94.36%) - LeetSync

--- easy/5-longest-palindromic-substring/longest-palindromic-substring.js
+++ easy/5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -2,30 +2,33 @@
  * @param {string} s
  * @return {string}
  */
-function longestPalindrome(s) {
-    if (!s || s.length < 1) return "";
+var longestPalindrome = function(s) {
+    if (!s || s.length < 1) return ""
 
-    let start = 0;
-    let end = 0;
+    let start = 0
+    let end = 0
 
     for (let i = 0; i < s.length; i++) {
-        const len1 = expandAroundCenter(s, i, i);     // центр — один символ (нечётная длина)
-        const len2 = expandAroundCenter(s, i, i + 1); // центр — два символа (чётная длина)
-        const len = Math.max(len1, len2);
+        const len1 = expandFromMiddle(s, i, i)
+        const len2 = expandFromMiddle(s, i, i+1)
+        const len = Math.max(len1, len2)
 
         if (len > end - start) {
             start = i - Math.floor((len - 1) / 2);
             end = i + Math.floor(len / 2);
         }
+
+
     }
+        return s.substring(start, end + 1)
 
-    return s.substring(start, end + 1);
-}
+    
+};
 
-function expandAroundCenter(s, left, right) {
+var expandFromMiddle = function(s, left, right) {
     while (left >= 0 && right < s.length && s[left] === s[right]) {
-        left--;
-        right++;
+        left--
+        right++
     }
-    return right - left - 1; // длина палиндрома
+    return right - left - 1
 }
